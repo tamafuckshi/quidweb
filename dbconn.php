2f3e1b89f9436fb75e69fdf0eca0a498a55086e4
tamafuckshi <alphapichoopapi@gmail.com>
2024-11-17 16:09:42 +0800

    d

--- dbconn.php
+++ dbconn.php
@@ -1,13 +1,14 @@
 <?php
-$host = 'localhost';
-$dbname = 'quidDB';
+$dsn = 'mysql:host=127.0.0.1;dbname=quidDB;charset=utf8mb4';
 $username = 'root';
 $password = 'admin';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO($dsn, $username, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
 } catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
+    die('Connection failed: ' . $e->getMessage());
 }
 ?>
