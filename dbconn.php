d613ffcd0dab3067f74fa0e4ff3f695f3e819874
tamafuckshi <alphapichoopapi@gmail.com>
2024-11-17 14:51:37 +0800

    w

--- dbconn.php
+++ dbconn.php
@@ -1,6 +1,6 @@
 <?php   
 try {
-    $pdo = new PDO("mysql:host=localhost;dbname=quidDB", "root", "");
+    $pdo = new PDO("mysql:host=localhost;dbname=quidDB", "root", "admin");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully!";
 } catch (PDOException $e) {
